4170327b1ab94107fef815ef7521cc5055c011b5
divya031090 <64855302+divya031090@users.noreply.github.com>
2025-09-24 22:15:44 -0400

    Create add_smoke.py

---
+++ add_smoke.py
@@ -0,0 +1,5 @@
+def test_import():
+    import src.idoselt.data as data
+    import src.idoselt.preprocess as preprocess
+    import src.idoselt.model as model
+    import src.idoselt.train as train
